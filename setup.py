812094943b6845ae3db82fb0d7e9eaef967e3894
Davi Ortega <ortegad@caltech.edu>
2016-12-21 18:58:39 -0800

    cleaning setup and trying to configure coveralls

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -17,6 +18,17 @@ requirements = [
 test_requirements = [
     # TODO: put package test requirements here
 ]
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        pytest.main(self.test_args)
+
 
 setup(
     name='bitk3',
@@ -49,14 +61,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    test_suite='tests',
-    tests_require=test_requirements
+    #test_suite='tests',
+    tests_require=['pytest'],
+    cmdclass = {'test': PyTest}
 )
